14e572e02a96c315047f8d700231e6c37d25ff65
Kim Fransson <kim.fransson@sigmatechnology.se>
2023-11-15 22:40:17 +0100

    extending tailwind to match design system

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <p className="headline-xl">Lorem Ipsum</p>
+      <p className="headline-lg">Lorem Ipsum</p>
+      <p className="headline-md">Lorem Ipsum</p>
+      <p className="headline-sm">Lorem Ipsum</p>
+
+      <p className="body">Lorem Ipsum</p>
+      <p className="body-2">Lorem Ipsum</p>
+      <p className="body-3">Lorem Ipsum</p>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,65 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: "#20233E",
+        gray: {
+          100: "#F5F5F5",
+          900: "#212121",
+        },
+        indigo: {
+          50: "#E8EAF6",
+          400: "#5C6BC0",
+        },
+      },
+      opacity: {
+        8: ".08",
+        24: ".24",
+        32: ".32",
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    // https://github.com/tailwindlabs/tailwindcss-intellisense/issues/227#issuecomment-1139895799
+    ({ addUtilities }) => {
+      addUtilities({
+        ".headline-xl": {
+          "font-size": "3.938rem",
+          "font-weight": "700",
+          "letter-spacing": "0.016em",
+        },
+        ".headline-lg": {
+          "font-size": "2.188rem",
+          "font-weight": "600",
+          "letter-spacing": "0.016em",
+        },
+        ".headline-md": {
+          "font-size": "1.625rem",
+          "font-weight": "500",
+          "letter-spacing": "0.016em",
+        },
+        ".headline-sm": {
+          "font-size": "1.313rem",
+          "font-weight": "600",
+          "letter-spacing": "0.009em",
+        },
+        ".body": {
+          "font-s": "1rem",
+          "font-weight": "400",
+          "letter-spacing": "0.009em",
+        },
+        ".body-2": {
+          "font-s": "1rem",
+          "font-weight": "500",
+          "letter-spacing": "0.009em",
+        },
+        ".body-3": {
+          "font-s": "1rem",
+          "font-weight": "700",
+          "letter-spacing": "0.016em",
+        },
+      });
+    },
+  ],
 };
