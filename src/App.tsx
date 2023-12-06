d0b6fc082db465713fadc36e331465ff2d2f7a10
Kim Fransson <kim.fransson@sigmatechnology.se>
2023-12-06 12:14:31 +0100

    moved geolocation button for enable automatic search

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import { TempUnitSwitcher } from "./components/TempUnitSwitcher/TempUnitSwitcher
 import { TemperaturePreferenceContext } from "./context/TemperaturePreferenceContext";
 import { ErrorBoundary } from "react-error-boundary";
 import { PageError } from "./components/PageError";
+import { GeoLocationButton } from "./components/GeoLocationButton";
 
 export default function App() {
   const [currentLocation, setCurrentLocation] = useLocalStorage<
@@ -42,10 +43,17 @@ export default function App() {
     <main className="mx-auto flex min-h-screen max-w-6xl flex-col gap-14 px-10 pb-12 pt-8">
       <header className="grid grid-cols-[1fr_min-content] items-center gap-4 sm:flex sm:gap-10">
         <Logo className="shrink-0 justify-self-end" />
-        <LocationSearchBar
-          onLocationSelected={setCurrentLocation}
-          className="col-span-2 flex-1"
-        />
+
+        <div className="relative col-span-2 flex-1">
+          <LocationSearchBar onLocationSelected={setCurrentLocation} />
+          <GeoLocationButton
+            onGeoLocationApproval={(lat, lon) => {
+              trigger(`${lat}, ${lon}`);
+            }}
+            className="absolute right-4 top-1/2 -translate-y-1/2"
+          />
+        </div>
+
         <TempUnitSwitcher
           aria-label="switch unit between fahrenheit and celsius"
           isSelected={preferCelsius}
--- src/components/LocationSearchBar/LocationSearchBar.tsx
+++ src/components/LocationSearchBar/LocationSearchBar.tsx
@@ -3,8 +3,6 @@ import { LocationData, searchByQuery } from "../../api";
 import { Autocomplete } from "./Autocomplete";
 import { useDebounce } from "@uidotdev/usehooks";
 import { useEffect, useState } from "react";
-import { GeoLocationButton } from "../GeoLocationButton";
-import { twMerge } from "tailwind-merge";
 export interface LocationSearchBarProps {
   onLocationSelected: (location: LocationData) => void;
   className?: string;
@@ -12,7 +10,6 @@ export interface LocationSearchBarProps {
 
 export const LocationSearchBar = ({
   onLocationSelected,
-  className,
 }: LocationSearchBarProps) => {
   const [searchQuery, setSearchQuery] = useState<string>("");
   const debouncedSearchQuery = useDebounce(searchQuery, 300);
@@ -30,23 +27,14 @@ export const LocationSearchBar = ({
   }, [debouncedSearchQuery, mutate]);
 
   return (
-    <div className={twMerge("relative", className)}>
-      <Autocomplete
-        searchResults={locations}
-        onSearchQueryChanged={setSearchQuery}
-        searchQuery={searchQuery}
-        isLoading={isLoading}
-        aria-label="Search location for weather forecast"
-        placeholder="Search for cities"
-        onLocationSelected={onLocationSelected}
-      />
-
-      <GeoLocationButton
-        onGeoLocationApproval={(lat, lon) => {
-          setSearchQuery(`${lat}, ${lon}`);
-        }}
-        className="absolute right-4 top-1/2 -translate-y-1/2"
-      />
-    </div>
+    <Autocomplete
+      searchResults={locations}
+      onSearchQueryChanged={setSearchQuery}
+      searchQuery={searchQuery}
+      isLoading={isLoading}
+      aria-label="Search location for weather forecast"
+      placeholder="Search for cities"
+      onLocationSelected={onLocationSelected}
+    />
   );
 };
